0a418b38ed7b8bde967c34ae7e4224e8250e6b37
Facooya <facooya@gmail.com>
2025-04-26 11:53:00 +0900

    build: archive v1.1.3-alpha

--- related_menu_list_v1.1.3a.js
+++ related_menu_list_v1.1.3a.js
@@ -7,18 +7,18 @@
  */
 /* === Variable === */
 /* --- Container Name ---!!! Check List A !!! */
-var related_menu_container_name_list=["[V1.1.2A] Chapter 1","[V1.1.2A] Chapter 2","[V1.1.2A] Chapter 3"];
+var related_menu_container_name_list=["[V1.1.3A] Chapter 1","[V1.1.3A] Chapter 2","[V1.1.3A] Chapter 3"];
 /* ------------------------------ */
-/* --- Item Name List ---!!! Check List B !!! *//* +++ CN == Container Name +++ */
-var _CN1_item_name_list=["[Chapter 1] Version 1.1.2 Alpha","[Chapter 1] Item 1-2"];
+/* --- Item Name List ---!!! Check List B !!! *//* + CN == Container Name + */
+var _CN1_item_name_list=["[Chapter 1] Version 1.1.3 Alpha","[Chapter 1] Item 1-2"];
 var _CN2_item_name_list=["[Chapter 2] Item 2-1"];
 var _CN3_item_name_list=["[Chapter 3] Item 2-1"];
 var related_menu_item_name_list=[_CN1_item_name_list,_CN2_item_name_list,_CN3_item_name_list];
 /* ------------------------------ */
-/* --- Item Link List ---!!! Check List C !!! *//* +++ CN == Container Name +++ */
-var _CN1_item_link_list=["./document_page_v1.1.2a.html","./document_page_v1.1.2a.html"];
-var _CN2_item_link_list=["./document_page_v1.1.2a.html"];
-var _CN3_item_link_list=["./document_page_v1.1.2a.html"];
+/* --- Item Link List ---!!! Check List C !!! *//* + CN == Container Name + */
+var _CN1_item_link_list=["./document_page_v1.1.3a.html","./document_page_v1.1.3a.html"];
+var _CN2_item_link_list=["./document_page_v1.1.3a.html"];
+var _CN3_item_link_list=["./document_page_v1.1.3a.html"];
 var related_menu_item_link_list=[_CN1_item_link_list,_CN2_item_link_list,_CN3_item_link_list];
 /* ------------------------------ */
 /* --- Html Generate ---!!! Check List D !!! */
@@ -60,7 +60,7 @@ for(let i=0;i<related_menu_item_name_list.length;i++){
 }
 /* ============================== */
 /* === Html Code Reference === */
-/* +++ RM == Related Menu +++
+/* + RM == Related Menu +
 <nav id="related_menu_section">
   --- JS Generate ---
   <ul class="related_menu_container">
--- top_bar_graphic_v1.1.3a.js
+++ top_bar_graphic_v1.1.3a.js
@@ -5,18 +5,29 @@
  *
  * Top bar
  */
-/*=== Variable ===*/
-var top_bar;
+/* === Variable === */
+/* --- Html Generate --- */
+/* --- Related Menu - */
 var tbg_related_menu_graphic;
 var tbg_related_menu_line_graphic=[];
+/* --- Facooya Name - */
 var tbg_facooya;
 var tbg_facooya_link;
+/* --- Scroll Menu - */
+var tbg_scroll_menu_graphic;
+var tbg_scroll_menu_line_graphic=[];
+/* --- Facooya Menu - */
 var tbg_facooya_menu_graphic;
 var tbg_facooya_menu_line_graphic=[];
-/*==============================*/
-/*=== Code ===*/
-/*--- TBG Related Menu Graphic ---*/
-top_bar=document.getElementById("top_bar");
+/* ------------------------------ */
+/* --- Find Id --- */
+//var top_bar=document.getElementById("top_bar");
+var top_bar=document.getElementById("top_bar");
+var code_style=document.getElementById("code_style");
+/* ------------------------------ */
+/* ============================== */
+/* === Code === */
+/* --- TBG Related Menu Graphic --- */
 tbg_related_menu_graphic=document.createElement("div");
 tbg_related_menu_graphic.setAttribute("id","tbg_related_menu_graphic");
 for(let i=0;i<3;i++){
@@ -25,8 +36,8 @@ for(let i=0;i<3;i++){
   tbg_related_menu_graphic.appendChild(tbg_related_menu_line_graphic[i]);
 }
 top_bar.appendChild(tbg_related_menu_graphic);
-/*------------------------------*/
-/*--- TBG Facooya Name ---*/
+/* ------------------------------ */
+/* --- TBG Facooya Name --- */
 tbg_facooya=document.createElement("div");
 tbg_facooya.setAttribute("id","tbg_facooya");
 tbg_facooya_link=document.createElement("a");
@@ -35,11 +46,20 @@ tbg_facooya_link.setAttribute("href","/");
 tbg_facooya_link.innerHTML="Facooya";
 tbg_facooya.appendChild(tbg_facooya_link);
 top_bar.appendChild(tbg_facooya);
-/*------------------------------*/
-/*--- TBG Scroll Menu Graphic ---*/
-/*!!!! ADD Scroll Menu & Main page Check */
-/*------------------------------*/
-/*--- TBG Facooya Menu Graphic ---*/
+/* ------------------------------ */
+/* --- TBG Scroll Menu Graphic --- */
+if(code_style){ /* - document_page ? Generate Scroll Menu - */
+  tbg_scroll_menu_graphic=document.createElement("div");
+  tbg_scroll_menu_graphic.setAttribute("id","tbg_scroll_menu_graphic");
+  for(let i=0;i<2;i++){
+    tbg_scroll_menu_line_graphic[i]=document.createElement("span");
+    tbg_scroll_menu_line_graphic[i].setAttribute("class","tbg_scroll_menu_line_graphic");
+    tbg_scroll_menu_graphic.appendChild(tbg_scroll_menu_line_graphic[i]);
+  }
+  top_bar.appendChild(tbg_scroll_menu_graphic);
+}
+/* ------------------------------ */
+/* --- TBG Facooya Menu Graphic --- */
 tbg_facooya_menu_graphic=document.createElement("div");
 tbg_facooya_menu_graphic.setAttribute("id","tbg_facooya_menu_graphic");
 for(let i=0;i<3;i++){
@@ -48,10 +68,10 @@ for(let i=0;i<3;i++){
   tbg_facooya_menu_graphic.appendChild(tbg_facooya_menu_line_graphic[i]);
 }
 top_bar.appendChild(tbg_facooya_menu_graphic);
-/*------------------------------*/
-/*==============================*/
-/*=== Html Code Reference ===*/
-/* tbg = Top bar graphic
+/* ------------------------------ */
+/* ============================== */
+/* === Html Code Reference === */
+/* + tbg = Top bar graphic +
 <div id="tbg_related_menu_graphic">
   <span class="tbg_related_menu_line_graphic"></span>
   <span class="tbg_related_menu_line_graphic"></span>
@@ -60,14 +80,16 @@ top_bar.appendChild(tbg_facooya_menu_graphic);
 <div id="tbg_facooya">
   <a id="tbg_facooya_link" href="/">Facooya</a>
 </div>
-<div id="tbg_scroll_menu_graphic">
-  <span class="tbg_scroll_menu_line_graphic"></span>
-  <span class="tbg_scroll_menu_line_graphic"></span>
-</div>
+if(document_page){
+  <div id="tbg_scroll_menu_graphic">
+    <span class="tbg_scroll_menu_line_graphic"></span>
+    <span class="tbg_scroll_menu_line_graphic"></span>
+  </div>
+}
 <div id="tbg_facooya_menu_graphic">
   <span class="tbg_facooya_menu_line_graphic"></span>
   <span class="tbg_facooya_menu_line_graphic"></span>
   <span class="tbg_facooya_menu_line_graphic"></span>
 </div>
 */
-/*==============================*/
+/* ============================== */
