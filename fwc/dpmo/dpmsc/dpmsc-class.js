7172488dd0cfa7eab9e840be4b2386bcf9884745
Facooya <facooya@gmail.com>
2025-04-29 15:50:56 +0900

    build: archive v1.1.22-alpha.2

--- fwc/dpmo/dpmsc/dpmsc-class.js
+++ fwc/dpmo/dpmsc/dpmsc-class.js
@@ -18,12 +18,11 @@ class DpmscController {
   }
   static load() {
     DpmscManager.load();
-    DpmscManager.event(true);
+    DpmscManager.loadEvent();
   }
   static resizeDisplay() {
     DpmscManager.resizeDisplay();
-    DpmscManager.event(false);
-    DpmscManager.event();
+    /* DpmscManager.resizeEvent(true); */
   }
 }
 class DpmscManager {
@@ -31,45 +30,33 @@ class DpmscManager {
 
   }
   static load() {
-    DpmscHandler.getText();
-    DpmscHandler.getLine();
+    /* DpmscHandler.getText(); */
+    DpmscConfig.replaceCodeColor();
+    /* DpmscHandler.getLine(); */
+    DpmscConfig.lineItemGenerate();
+    DpmscHandler.lineWrapper();
   }
   static resizeDisplay() {
 
   }
-  static event(isActive) {
-    const {
-      dpmscTeraTlo
-    } = DpmscConfig.getDpmscTloGroup();
-    /*  */
-    let displayType = BlfConfig.previousDisplayType;
-    let eventListenerType = "removeEventListener";
-    if (isActive) {
-      displayType = BlfConfig.currentDisplayType;
-      eventListenerType = "addEventListener";
+  static loadEvent() {
+    /* document.addEventListener("copy", DpmscHandler.copyEvent); */
+    const dpmscTitleLink = document.querySelectorAll(".dpmsc-title-link");
+    for (let tli = 0; tli < dpmscTitleLink.length; tli++) {
+      dpmscTitleLink[tli].addEventListener(
+        "click",
+        DpmscHandler.dpmscTitleLink
+      );
     }
-    /*  */
-    switch (displayType) {
-      case 1: {
-        for (let tbi = 0; tbi < dpmscTeraTlo.length; tbi++) {
-          dpmscTeraTlo[tbi][eventListenerType](
-            "click",
-            DpmscHandler.mdtDpmscTeraTlo
-          );
-        }
-        break;
-      }
-      case 2: {
-        break;
-      }
-      case 3: {
-        break;
-      }
-    }
+    /* const fold = document.querySelectorAll(".fold");
+    fold[0].addEventListener(
+      "click",
+      DpmscHandler.fold
+    ); */
   }
 }
 class DpmscHandler {
-  static mdtDpmscTeraTlo(eventData) {
+  static dpmscTitleLink(eventData) {
     const {
       eventCurrentTarget
     } = BlfUtil.getEventData(eventData);
@@ -79,30 +66,14 @@ class DpmscHandler {
     history.replaceState(null, null, hash);
     hashElement.scrollIntoView();
   }
-  static getText() {
-    /* const {
-      dpmscZ
-    } = DpmscConfig.getDpmscZbGroup();
-    const {
-      dpmscGigaText
-    } = DpmscConfig.getDpmscPbGroup(2); */
+  /* ===== */
+  /* static getText() {
     const dpmscViewerCode = document.querySelectorAll(".dpmsc-viewer-code");
     let getText = dpmscViewerCode[0].innerHTML;
-    /*  */
     let regExp = /(".*?(?<!\\)"|\/\*[\s\S]*?\*\/|'.*?')/;
-    /* getText = getText.replace(regExp, "<span class='green'>$&</span>");
-    regExp = /(\/\*[\s\S]*?\*\/)/g;
-    getText = getText.replace(regExp, "<span class='comment'>$&</span>"); */
     let part = getText.split(regExp);
     for (let i = 0; i < part.length; i++) {
       if (part[i][0] === "\"") {
-        /* getText = getText.replace(part[i], "<span class='green'>$&</span>"); */
-        /* for (let j = 0; j < part[i].length; j++) {
-          console.log(part[i][j]);
-          if (part[i][j] == "\\") {
-            part[i][j].replace(part[i][j], "<span class='green-bold'>$&</span>");
-          }
-        } */
         part[i] = part[i].replace(/\\./g, "<span class='green-bold'>$&</span>");
         part[i] = part[i].replace(/%./g, "<span class='green-bold'>$&</span>");
         part[i] = part[i].replace(part[i], "<span class='green'>$&</span>");
@@ -118,35 +89,149 @@ class DpmscHandler {
         part[i] = part[i].replace(/\b(0[xX][0-9a-fA-F]+|\d+(\.\d+f?)?)\b/g, "<span class='blue-light'>$&</span>");
       }
     }
-    /* part.join(""); */
-    /* part.join(""); */
-    /*  */
-    /* regExp = /\bint\b/g;
-    let setTag = getText.replace(regExp, "<span class='orange'>$&</span>");
-
-    regExp = /\bprintf\b/g;
-    setTag = setTag.replace(regExp, "<span class='blue'>$&</span>")
-    dpmscGigaText[0].innerHTML = setTag; */
     dpmscViewerCode[0].innerHTML = part.join("");
   }
   static getLine() {
-    /* const {
-      dpmscGigaText
-    } = DpmscConfig.getDpmscPbGroup(2); */
+    const fragment = document.createDocumentFragment();
     const dpmscLineList = document.querySelectorAll(".dpmsc-line-list");
     const dpmscViewerCode = document.querySelectorAll(".dpmsc-viewer-code");
-    let getText = dpmscViewerCode[0].textContent;
-    const parts = getText.split("\n");
-    for (let i = 0; i < parts.length; i++) {
-      const dpmscLineItem = document.createElement("li");
-      const dpmscLineNumber = document.createElement("span");
-      dpmscLineItem.setAttribute("class", "dpmsc-line-item");
-      dpmscLineNumber.setAttribute("class", "dpmsc-line-number");
-      dpmscLineNumber.textContent = (i + 1).toString();
-      dpmscLineItem.append(dpmscLineNumber);
-      dpmscLineList[0].append(dpmscLineItem);
+
+    for (let vci = 0; vci < dpmscViewerCode.length; vci++) {
+      const getText = dpmscViewerCode[vci].textContent;
+      const lineCount = getText.split("\n");
+      for (let li = 0; li < lineCount.length; li++) {
+        const dpmscLineItem = document.createElement("li");
+        dpmscLineItem.setAttribute("class", "dpmsc-line-item");
+        const dpmscLineNumber = document.createElement("span");
+        dpmscLineNumber.setAttribute("class", "dpmsc-line-number");
+        dpmscLineNumber.textContent = (li + 1).toString();
+        /*  
+        dpmscLineItem.append(dpmscLineNumber);
+        fragment.append(dpmscLineItem);
+      }
+      dpmscLineList[vci].append(fragment);
+    } 
+  } */
+  /* static fold() {
+    const fold = document.querySelectorAll(".fold");
+    if (fold[0].isFold) {
+      fold[0].style.overflow = "";
+      fold[0].style.height = "";
+      fold[0].isFold = false;
+    } else {
+      fold[0].style.overflow = "hidden";
+      fold[0].style.height = "1.5rem";
+      fold[0].isFold = true;
     }
+  } */
+  static lineWrapper() {
+    const dpmscViewerCode = document.querySelectorAll(".dpmsc-viewer-code");
+    dpmscViewerCode[1].original = dpmscViewerCode[1].textContent;
+    let innerCode = dpmscViewerCode[1].innerHTML;
+    const lines = innerCode.split("\n");
+    for (let li = 0; li < lines.length; li++) {
+      lines[li] = lines[li].replace(lines[li], "<div class=\"line\">$&</div>");
+    }
+    for (let li = 0; li < lines.length; li++) {
+      lines[li] = lines[li].replace(/void testFoldFunction/, "<span class=\"test-fold\">$&</span>");
+      /* lines[li] = lines[li].replace("{", "{ <span class=\"skip\">...</span> }"); */
+      /* if (lines[li].match("void testFoldFunction")) {
+        /* lines[li].addEventListener("click", this.lineFold); 
+        const foldLine = document.querySelectorAll(".line");
+        console.log(foldLine);
+        foldLine[li].addEventListener("click", DpmscHandler.lineFold);
+        for (let i = li; i < lines.length; i++) {
+          if (lines[i].match("}")) {
+            console.log(li, i);
+            break;
+          }
+        }
+      } */
+    }
+    /* dpmscViewerCode[1].innerHTML = lines.join(""); */
+    /* innerCode = lines.join(""); */
+    /* if (innerCode.match("testFoldFunction")) {
+      console.log("testFold");
+    } */
+    innerCode = lines.join("");
+    dpmscViewerCode[1].innerHTML = innerCode;
+    /*  */
+    const foldLine = document.querySelectorAll(".line");
+    for (let i = 0; i < foldLine.length; i++) {
+      const text = foldLine[i].innerHTML;
+      foldLine[i].index = i;
+      if (text.match(/testFoldFunction\(\) \{$/g)) {
+        foldLine[i].i = i;
+        for (let j = foldLine[i].i; j < foldLine.length; j++) {
+          const text2 = foldLine[j].innerHTML;
+          if (text2.match("}")) {
+            foldLine[i].j = j;
+            break;
+          }
+        }
+        foldLine[0].isFold = false;
+        foldLine[i].addEventListener("click", this.lineFold);
+      }
+    }
+    /* if (innerCode.match("void testFoldFunction")) {
+      foldLine[0].addEventListener();
+    } */
   }
+  static lineFold() {
+    const foldLine = document.querySelectorAll(".line");
+    /* const dpmscViewerCode = document.querySelectorAll(".dpmsc-viewer-code"); */
+
+    /* let getText = dpmscViewerCode[1].innerHTML;
+    let parts = getText.split("");
+    for (let pi = 0; pi < parts.length; pi++) {
+      if (parts[pi] === "{") {
+        parts.splice(pi+1, 0, "abc");
+        break;
+      }
+    }
+    parts = parts.join(""); */
+    
+    if (foldLine[0].isFold) {
+      for (let i = 0; i < foldLine.length; i++) {
+        if (foldLine[i].i) {
+          for (let j = i+1; j <= foldLine[i].j; j++) {
+            foldLine[j].style.display = "";
+          }
+          foldLine[i].innerHTML = foldLine[i].original;
+        }
+      }
+      foldLine[0].isFold = false;
+    } else {
+      for (let i = 0; i < foldLine.length; i++) {
+        if (foldLine[i].i) {
+          for (let j = i+1; j <= foldLine[i].j; j++) {
+            foldLine[j].style.display = "none";
+          }
+          let getText = foldLine[i].innerHTML;
+          foldLine[i].original = getText;
+          getText = getText.replace(/{.*/, "{ <span class=\"fold\">...</span> }");
+          foldLine[i].innerHTML = getText;
+          break;
+        }
+      }
+      foldLine[0].isFold = true;
+    }
+    /* dpmscViewerCode[1].innerHTML = parts; */
+  }
+
+  /* static copyEvent(eventData) {
+    const select = window.getSelection();
+    const range = select.getRangeAt(0);
+    const clone = range.cloneContents();
+    const foldLine = document.querySelectorAll(".fold-line");
+    const fold = clone.querySelector(".fold");
+    /*  
+    if (fold) {
+      eventData.preventDefault();
+      let text = foldLine[0];
+      eventData.clipboardData.setData("text/plain", fold.original);
+    }
+  } */
 }
 /*  */
 export {
--- fwc/dpmo/dpmsc/dpmsc-config.js
+++ fwc/dpmo/dpmsc/dpmsc-config.js
@@ -8,282 +8,98 @@ import {
 } from "../../fwc-hub.js";
 /*  */
 class DpmscConfigData {
-
+  static textData = {
+    split: /(".*?(?<!\\)"|\/\*[\s\S]*?\*\/|'.*?')/,
+    newline: /\n/,
+    backslash: /\\./g,
+    percent: /%./g
+  };
+  static findData = {
+    /* splitText: /(".*?(?<!\\)"|\/\*[\s\S]*?\*\/|'.*?')/, */
+    dataType: /\b(void|int|float|char|return)\b/g,
+    systemFunction: /\b(if|else|switch|while|for)\b/g,
+    stdioFunction: /\bprintf\b/g,
+    number: /\b(0[xX][0-9a-fA-F]+|\d+(\.\d+f?)?)\b/g
+  };
+  static replaceData = {
+    comment: "<span class=\"comment\">$&</span>",
+    number: "<span class=\"number\">$&</span>",
+    text: "<span class=\"text\">$&</span>",
+    textBold: "<span class=\"text-bold\">$&</span>",
+    orange: "<span class=\"orange\">$&</span>",
+    blue: "<span class=\"blue\">$&</span>",
+    yellow: "<span class=\"yellow\">$&</span>"
+  };
 }
 class DpmscConfigElement {
-  static dpmscRoot = [
-    {
-      elementId: "dpmscR",
-      query: {
-        querySelector: ".dpmsc-r",
-        queryType: "single"
-      }
-    }
-  ];
-  static dpmsc = [
-    {
-      elementId: "dpmsc",
-      cacheId: "dpmsc",
-      query: {
-        querySelector: ".dpmsc",
-        queryType: "single"
-      }
-    }
-  ];
-  static dpmscContent = [
-    {
-      elementId: "dpmscContentList",
-      query: {
-        querySelector: ".dpmsc-content-list",
-        queryType: "single"
-      }
-    },
-    {
-      elementId: "dpmscContentItem",
-      query: {
-        querySelector: ".dpmsc-content-item",
-        queryType: "all"
-      }
-    }
-  ];
-  static dpmscTitle = [
-    {
-      elementId: "dpmscTitleContainer",
-      query: {
-        querySelector: ".dpmsc-title-container",
-        queryType: "all"
-      }
-    },
-    {
-      elementId: "dpmscTitleHeading",
-      query: {
-        querySelector: ".dpmsc-title-heading",
-        queryType: "all"
-      }
-    },
-    {
-      elementId: "dpmscTitleLink",
-      query: {
-        querySelector: ".dpmsc-title-link",
-        queryType: "all"
-      }
-    },
-    {
-      elementId: "dpmscTitleText",
-      query: {
-        querySelector: ".dpmsc-title-text",
-        queryType: "all"
-      }
-    }
-  ];
-  static dpmscViewer = [
-    {
-      elementId: "dpmscViewerList",
-      query: {
-        querySelector: ".dpmsc-viewer-list",
-        queryType: "all"
-      }
-    },
-    {
-      elementId: "dpmscViewerItem",
-      query: {
-        querySelector: ".dpmsc-viewer-item",
-        queryType: "all"
-      }
-    },
-    {
-      elementId: "dpmscViewerName",
-      query: {
-        querySelector: ".dpmsc-viewer-name",
-        queryType: "all"
-      }
-    },
-    {
-      elementId: "dpmscViewerContainer",
-      query: {
-        querySelector: ".dpmsc-viewer-container",
-        queryType: "all"
-      }
-    },
-    {
-      elementId: "dpmscViewer",
-      query: {
-        querySelector: ".dpmsc-viewer",
-        queryType: "all"
-      }
-    }
-  ];
-  /*  */
-  static dpmscGroup = [
-    {
-      elementId: "dpmscY",
-      query: {
-        querySelector: ".dpmsc-y",
-        queryType: "single"
-      }
-    }
-  ];
-  static dpmscZbGroup = [
-    {
-      elementId: "dpmscZ",
-      query: {
-        querySelector: ".dpmsc-z",
-        queryType: "all"
-      }
-    },
-    {
-      elementId: "dpmscE",
-      query: {
-        querySelector: ".dpmsc-e",
-        queryType: "all"
-      }
-    }
-  ];
-  static dpmscTloGroup = [
-    {
-      elementId: "dpmscExaTlo",
-      query: {
-        querySelector: ".dpmsc-e-tlo",
-        queryType: "all"
-      }
-    },
-    {
-      elementId: "dpmscPetaTlo",
-      query: {
-        querySelector: ".dpmsc-p-tlo",
-        queryType: "all"
-      }
-    },
-    {
-      elementId: "dpmscTeraTlo",
-      query: {
-        querySelector: ".dpmsc-t-tlo",
-        queryType: "all"
-      }
-    },
-    {
-      elementId: "dpmscGigaTloText",
-      query: {
-        querySelector: ".dpmsc-g-tlo-text",
-        queryType: "all"
-      }
-    }
-  ];
-  static dpmscPbGroup = [
-    {
-      elementId: "dpmscP",
-      query: {
-        querySelector: ".dpmsc-p",
-        queryType: "all"
-      }
-    },
-    {
-      elementId: "dpmscTeraText",
-      query: {
-        querySelector: ".dpmsc-t-text",
-        queryType: "all"
-      }
-    },
-    {
-      elementId: "dpmscT",
-      query: {
-        querySelector: ".dpmsc-t",
-        queryType: "all"
-      }
-    },
-    {
-      elementId: "dpmscGigaText",
-      query: {
-        querySelector: ".dpmsc-g-text",
-        queryType: "all"
-      }
-    }
-  ];
+
 }
 class DpmscConfigGet {
-  static getDpmsc() {
-    const dpmscElement = document.querySelector(".dpmsc");
-    return dpmscElement;
-  }
-  static getDpmscContent() {
-    const { dpmsc } = DpmscConfigGet.getDpmsc();
 
-    return 0;
+}
+class DpmscConfigManager {
+  static lineItemGenerate() {
+    const fragment = document.createDocumentFragment();
+    const dpmscLineList = document.querySelectorAll(".dpmsc-line-list");
+    const dpmscViewerCode = document.querySelectorAll(".dpmsc-viewer-code");
+    /*  */
+    for (let vci = 0; vci < dpmscViewerCode.length; vci++) {
+      const getText = dpmscViewerCode[vci].textContent;
+      const lineCount = getText.split("\n");
+      for (let li = 0; li < lineCount.length; li++) {
+        const dpmscLineItem = document.createElement("li");
+        dpmscLineItem.setAttribute("class", "dpmsc-line-item");
+        const dpmscLineNumber = document.createElement("span");
+        dpmscLineNumber.setAttribute("class", "dpmsc-line-number");
+        dpmscLineNumber.textContent = (li + 1).toString();
+        /*  */
+        dpmscLineItem.append(dpmscLineNumber);
+        fragment.append(dpmscLineItem);
+      }
+      dpmscLineList[vci].append(fragment);
+    }
   }
-  static getDpmscViewer() {
-    return 0;
-  }
-  /*  */
-  static getDpmscRoot() {
-    const getGroup = BlfUtil.getElementCache(
-      DpmscConfig.dpmscConfigCache,
-      DpmscConfigElement.dpmscRoot
-    );
-    this.getDpmsc();
-    return getGroup;
-  }
-  static getDpmscGroup() {
-    const {
-      dpmscR
-    } = DpmscConfigGet.getDpmscRoot();
-    const getGroup = BlfUtil.getElementCache(
-      DpmscConfig.dpmscConfigCache,
-      DpmscConfigElement.dpmscGroup,
-      dpmscR
-    );
-    return getGroup;
-  }
-  static getDpmscZbGroup() {
-    const {
-      dpmscR
-    } = DpmscConfigGet.getDpmscRoot();
-    const getGroup = BlfUtil.getElementCache(
-      DpmscConfig.dpmscConfigCache,
-      DpmscConfigElement.dpmscZbGroup,
-      dpmscR
-    );
-    return getGroup;
-  }
-  static getDpmscTloGroup() {
-    const {
-      dpmscR
-    } = DpmscConfigGet.getDpmscRoot();
-    const getGroup = BlfUtil.getElementCache(
-      DpmscConfig.dpmscConfigCache,
-      DpmscConfigElement.dpmscTloGroup,
-      dpmscR
-    );
-    return getGroup;
-  }
-  static getDpmscPbGroup(pIndex) {
-    const {
-      dpmscZ
-    } = DpmscConfigGet.getDpmscZbGroup();
-    const getGroup = BlfUtil.getElementCache(
-      DpmscConfig.dpmscConfigCache,
-      DpmscConfigElement.dpmscPbGroup,
-      dpmscZ[pIndex]
-    );
-    return getGroup;
+  static replaceCodeColor() {
+    const dpmscViewerCode = document.querySelectorAll(".dpmsc-viewer-code");
+    const textData = DpmscConfig.textData;
+    const findData = DpmscConfig.findData;
+    const replaceData = DpmscConfig.replaceData;
+    /*  */
+    /* const splitText = DpmscConfig.findData.splitText; */
+    for (let vci = 0; vci < dpmscViewerCode.length; vci++) {
+      const getText = dpmscViewerCode[vci].innerHTML;
+      const parts = getText.split(textData.split);
+      for (let pi = 0; pi < parts.length; pi++) {
+        if (parts[pi][0] === "\"") {
+          parts[pi] = parts[pi].replace(textData.backslash, replaceData.textBold);
+          parts[pi] = parts[pi].replace(textData.percent, replaceData.textBold);
+          parts[pi] = parts[pi].replace(parts[pi], replaceData.text);
+        } else if (parts[pi][0] === "'") {
+          parts[pi] = parts[pi].replace(textData.backslash, replaceData.textBold);
+          parts[pi] = parts[pi].replace(parts[pi], replaceData.text);
+        } else if (parts[pi][0] === "/" && parts[pi][1] === "*") {
+          parts[pi] = parts[pi].replace(parts[pi], replaceData.comment);
+        } else {
+          parts[pi] = parts[pi].replace(findData.dataType, replaceData.orange);
+          parts[pi] = parts[pi].replace(findData.systemFunction, replaceData.yellow);
+          parts[pi] = parts[pi].replace(findData.stdioFunction, replaceData.blue);
+          parts[pi] = parts[pi].replace(findData.number, replaceData.number);
+        }
+      }
+      dpmscViewerCode[vci].innerHTML = parts.join("");
+    }
   }
 }
 class DpmscConfig {
-  static dpmscConfigCache = {};
+  static textData = DpmscConfigData.textData;
+  static findData = DpmscConfigData.findData;
+  static replaceData = DpmscConfigData.replaceData;
   /* -------------------------------------------------- */
-  static getDpmscRoot() {
-    return DpmscConfigGet.getDpmscRoot();
+  static replaceCodeColor() {
+    DpmscConfigManager.replaceCodeColor();
   }
-  static getDpmscGroup() {
-    return DpmscConfigGet.getDpmscGroup();
-  }
-  static getDpmscZbGroup() {
-    return DpmscConfigGet.getDpmscZbGroup();
-  }
-  static getDpmscTloGroup() {
-    return DpmscConfigGet.getDpmscTloGroup();
-  }
-  static getDpmscPbGroup(pIndex) {
-    return DpmscConfigGet.getDpmscPbGroup(pIndex);
+  static lineItemGenerate() {
+    DpmscConfigManager.lineItemGenerate();
   }
 }
 /*  */
